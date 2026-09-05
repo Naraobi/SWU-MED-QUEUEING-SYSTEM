42a620983b142fb8006e995f77c00779b5487edb
Naraobi <naraekele14@gmail.com>
2026-09-06 00:17:49 +0800

    Tracker url updates

--- src/queue/pages/Patient/PatientView.jsx
+++ src/queue/pages/Patient/PatientView.jsx
@@ -359,7 +359,7 @@ function TicketScreen({
   onPrint,
   onSkipPrint
 }) {
-  const trackerUrl = `https://swu-med-queueing-system-1.onrender.com/tracker?ticket=${queueId}`;
+  const trackerUrl = `${window.location.origin}/tracker?ticket=${queueId}`;
   return (
     <Screen>
       <KioskHeader />
